79c981ab253001e5dfa811805108db26150f0254
Thura Naing <scm.thuranaing@gmail.com>
2022-08-14 17:08:04 +0630

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyCnuWHEEGl6rsZdu11PJxt90CTB6GNqi3w',
-    authDomain: 'scm-spinner-vote.herokuapp.com',
+    authDomain: 'group-chat-6fff3.web.app',
     databaseURL: 'https://group-chat-6fff3-default-rtdb.firebaseio.com',
     projectId: 'group-chat-6fff3',
     storageBucket: 'group-chat-6fff3.appspot.com',
     messagingSenderId: '161615095659',
-    appId: '1:161615095659:web:0b9497c5345ff4e095ce2e',
-    measurementId: 'G-RPVGD38BMR',
+    appId: '1:161615095659:web:05946823c10cc2f295ce2e',
+    measurementId: 'G-0TWKLSZNNE'
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCnuWHEEGl6rsZdu11PJxt90CTB6GNqi3w',
-    authDomain: 'scm-spinner-vote.herokuapp.com',
-    databaseURL: 'https://group-chat-6fff3-default-rtdb.firebaseio.com',
-    projectId: 'group-chat-6fff3',
-    storageBucket: 'group-chat-6fff3.appspot.com',
-    messagingSenderId: '161615095659',
-    appId: '1:161615095659:web:0b9497c5345ff4e095ce2e',
-    measurementId: 'G-RPVGD38BMR',
+  apiKey: "AIzaSyCnuWHEEGl6rsZdu11PJxt90CTB6GNqi3w",
+  authDomain: "group-chat-6fff3.web.app",
+  databaseURL: "https://group-chat-6fff3-default-rtdb.firebaseio.com",
+  projectId: "group-chat-6fff3",
+  storageBucket: "group-chat-6fff3.appspot.com",
+  messagingSenderId: "161615095659",
+  appId: "1:161615095659:web:05946823c10cc2f295ce2e",
+  measurementId: "G-0TWKLSZNNE"
   },
 };
 
